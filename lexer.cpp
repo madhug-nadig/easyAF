f1d2e5cd2a1013ec73cf8331ad896757785a8540
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-26 14:28:20 +0530

    Continue lexer

--- lexer.cpp
+++ lexer.cpp
@@ -26,6 +26,45 @@ int main(){
 	string key;
 	char in;
 	int symtabIndex = 0;
+	
+	while(true){
+		cin >> key;
+		string tempKey;
+		
+		int strB = 0;
+		while(strB < key.length()){
+			int i = strB;
+			// cout<<strB<<" "<<key.length()<<endl;
+			for(;strB<key.length();strB++){
+				if(key[strB] == ' ' ||key[strB] == ',' ||key[strB] == ';' ||key[strB] == '+' ||key[strB] == '-' ||key[strB] == '*' ||key[strB] == '/') break;
+			}
+			tempKey = key.substr(i,(strB-i));
+			cout << tempKey<<endl;
+			unordered_map<string,int>::iterator it = symtab.find(tempKey);
+			if(it == symtab.end()){
+				    cout << tempKey << " not found\n";
+				    symtab[tempKey] = symtabIndex++;
+				    cout << "added " << tempKey << " to symtab at index " << symtabIndex-1 << endl;
+				}
+			else
+			    cout << "Found " << it->first <<" with value "<< it->second << "\n";
+			cout<<"--------------------------------------------"<<endl;
 
+			if(key[strB] == ' ' ||key[strB] == ',' ||key[strB] == ';' ||key[strB] == '+' ||key[strB] == '-' ||key[strB] == '*' ||key[strB] == '/'){
+				string temp = key.substr(strB,1);
+				it = symtab.find(temp);
+				if(it == symtab.end()){
+					    cout << temp << " not found\n";
+					    symtab[temp] = symtabIndex++;
+					    cout << "added " << key[strB] << " to symtab at index " << symtabIndex-1 << endl;
+					}
+				else
+				    cout << "Found " << it->first <<" with value "<< it->second << "\n";
+				cout<<"--------------------------------------------"<<endl;
+			}
+			strB++;
+		}
+	}
+	
 	return 0;
 }
